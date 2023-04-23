350c5fdae8ff34f2e726220927db119b3e6162ca
YLC726 <g2002726@gmail.com>
2023-04-23 22:42:02 +0800

    final ver.

--- read.py
+++ read.py
@@ -4,11 +4,14 @@ with open ('reviews.txt','r') as f:
 	for line in f:
 	    data.append(line)
 	    count += 1
-	    if count % 10000 == 0:#%為除以後求於數
+	    if count % 10000 == 0:#%
 	        print('已讀取',len(data),'則留言')
 print('---------------------')
-print('總共有',len(data),'則留言')#印出總行數
-print('---------------------')
-print(data[0])#印出第0行
-print('---------------------')
-print(data[2])
+print('總共有', len(data) , '則留言')#印出總行數
+total = 0
+for i in range(len(data)):
+    total += len(data[i])
+
+average = total/len(data)
+
+print("平均留言長度為:", average)
